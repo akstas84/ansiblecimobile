520d0ba4bebeef5230b1bfd0fb9fe65dc9523132
Stanislav <akstas84@gmail.com>
2018-03-26 19:17:32 +0300

    modified yml and add tasks

--- android.sh
+++
@@ -1,1 +0,0 @@
-export PATH="/opt/android-sdk-linux/tools:${PATH}"
---
+++ envsdk.sh
@@ -0,0 +1,5 @@
+#export ANDROID_HOME=/opt/android/sdk
+#export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
+
+echo 'export ANDROID_HOME=/home/android-sdk-linux' >> ~/.bashrc
+echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.bashrc
