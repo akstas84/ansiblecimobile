be59070ac55b8af85c150bbefd039ef1953f82b1
Stanislav <akstas84@gmail.com>
2018-03-31 22:24:48 +0300

    yml review

--- envsdk.sh
+++
@@ -1,5 +0,0 @@
-#export ANDROID_HOME=/opt/android/sdk
-#export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-
-echo 'export ANDROID_HOME=/u01/android-sdk/' >> ~/.bashrc
-echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.bashrc
