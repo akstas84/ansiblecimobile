b6a41c112ef44369639f4eb713fdbc89a89c340f
Stanislav <akstas84@gmail.com>
2018-03-31 21:38:47 +0300

    midified yml

--- envsdk.sh
+++ envsdk.sh
@@ -1,5 +1,5 @@
 #export ANDROID_HOME=/opt/android/sdk
 #export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
-echo 'export ANDROID_HOME=/home/android-sdk-linux' >> ~/.bashrc
+echo 'export ANDROID_HOME=/u01/android-sdk/' >> ~/.bashrc
 echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools' >> ~/.bashrc
